07eed5f0d3d1e3d658de97a7e7cd54cb12bff45e
Hillary Ikwara <hillaryikwara193@gmail.com>
2023-10-20 12:22:22 +0300

    Navigation drawer scrolling effect

--- app/src/main/java/com/gwallaz/pdfreader/MainActivity.kt
+++ app/src/main/java/com/gwallaz/pdfreader/MainActivity.kt
@@ -3,17 +3,7 @@ package com.gwallaz.pdfreader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.gwallaz.pdfreader.ui.theme.PDFReaderTheme
-import com.gwallaz.pdfreader.user_interface.bottom_navigation.BottomBar
 import com.gwallaz.pdfreader.user_interface.bottom_navigation.Bottombar
-
 import com.gwallaz.pdfreader.user_interface.screens.AllFiles
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/gwallaz/pdfreader/user_interface/screens/AllFiles.kt
+++ app/src/main/java/com/gwallaz/pdfreader/user_interface/screens/AllFiles.kt
@@ -1,9 +1,32 @@
 package com.gwallaz.pdfreader.user_interface.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.ScrollScope
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.layout.windowInsetsEndWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Call
@@ -24,6 +47,8 @@ import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Send
 import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -48,9 +73,15 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -68,6 +99,9 @@ data class DrawerItems(
     fun AllFiles() {
 
     val items = listOf(
+        DrawerItems(
+            title = "PDF Reader"
+        ),
         DrawerItems(
             selecteditem = Icons.Filled.Email,
             title = "Browse PDF",
@@ -113,61 +147,107 @@ data class DrawerItems(
             title = "Version: 1.3.5c"
         )
     )
+
     val scrollBehaviour = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val drawerScope  = rememberCoroutineScope()
+    val backgroundAlpha = if (drawerState.isOpen) 0.3f else 1.0f
+    val backgroundColor = androidx.compose.ui.graphics.Color.Gray
+    val drawerScope = rememberCoroutineScope()
     var selectedItemIndex by rememberSaveable {
         mutableIntStateOf(0)
     }
+    val drawerWidth =
+        (LocalDensity.current.run { LocalConfiguration.current.screenWidthDp.dp * 3 / 4 })
+    val maxHeight = 700.dp
 
 
     ModalNavigationDrawer(
         drawerContent = {
-                        ModalDrawerSheet {
-                            Spacer(modifier = Modifier.height(16.dp))
-                            items.forEachIndexed{index, item ->
-                                NavigationDrawerItem(
-                                    label = {
-                                        item.title?.let { Text(text = it) }
-                                    },
-                                    selected = index == selectedItemIndex,
-                                    onClick = {
-                                        selectedItemIndex = index
-                                        drawerScope.launch {
-                                            drawerState.close()
-                                        }
-                                    },
-                                    icon = {
-                                        (if (index == selectedItemIndex){
-                                            item.selecteditem
-                                        }else{
-                                            item.unselecteditem
-                                        })?.let {
-                                            Icon(
-                                                imageVector = it,
-                                                contentDescription =item.title )
-                                        }
-                                    },
-                                    modifier = Modifier.
-                                            padding(NavigationDrawerItemDefaults.ItemPadding)
+            ModalDrawerSheet {
+                Spacer(
+                    modifier = Modifier
+                        .height(16.dp)
+                        .fillMaxWidth(0.7f)
+                        .alpha(0.3f)
+                )
 
-                                )
+                Column {
 
-                            }
+
+                    items.forEachIndexed { index, item ->
+                        NavigationDrawerItem(
+
+                            label = {
+                                item.title?.let { Text(text = it) }
+                                //when(item.title){
+                                //     "Settings" -> Divider(modifier = Modifier.height(2.dp).padding(16.dp), color = androidx.compose.ui.graphics.Color.LightGray)
+                                // }
+
+
+                            },
+                            selected = index == selectedItemIndex,
+                            onClick = {
+                                selectedItemIndex = index
+                                drawerScope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            icon = {
+                                (if (index == selectedItemIndex) {
+                                    item.selecteditem
+                                } else {
+                                    item.unselecteditem
+                                })?.let {
+                                    Icon(
+                                        imageVector = it,
+                                        contentDescription = item.title
+                                    )
+                                }
+                            },
+                            modifier = Modifier
+                                .padding(NavigationDrawerItemDefaults.ItemPadding)
+
+                                .fillMaxWidth(0.7f)
+
+
+                        )
+                        if (item.title in listOf("PDF Reader", "Privacy Policy")) {
+                            Divider(
+                                modifier = Modifier
+                                    .fillMaxWidth(0.7f)
+                                    .padding(start = 4.dp),
+                                color = androidx.compose.ui.graphics.Color.LightGray
+                            )
                         }
-        },
-        drawerState = drawerState
-        )
 
+
+                    }
+                }
+            }
+        },
+        drawerState = drawerState,
+        modifier = Modifier
+            .verticalScroll(rememberScrollState(), true)
+            .height(700.dp)
+
+
+    )
     {
+        Box {
+
+
         Scaffold(
-            modifier = Modifier.nestedScroll(scrollBehaviour.nestedScrollConnection),
+            modifier = Modifier
+                .nestedScroll(scrollBehaviour.nestedScrollConnection)
+                ,
+
             topBar = {
                 MediumTopAppBar(
                     colors = TopAppBarDefaults.mediumTopAppBarColors(
                         containerColor = MaterialTheme.colorScheme.primaryContainer,
                         titleContentColor = MaterialTheme.colorScheme.primary,
-                    ),
+
+                        ),
                     title = {
                         Text(
                             text = "All files",
@@ -208,9 +288,16 @@ data class DrawerItems(
             },
         ) {
 
-        }
+            }
+
     }
-}
+}}
+
+
+
+
+
+
 
 
 
